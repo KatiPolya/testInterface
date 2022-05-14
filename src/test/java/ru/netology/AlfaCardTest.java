ed231a213d5873cb2f02f3221139919b33e3e9e1
Катерина Полякова <katsiaryna.paliakova@gmail.com>
2022-05-14 22:44:30 +0200

    9

--- src/test/java/ru/netology/AlfaCardTest.java
+++ src/test/java/ru/netology/AlfaCardTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,6 +23,14 @@ class AlfaCardTest {
     @BeforeEach
     void setupTest() {
         driver = new ChromeDriver();
+    }
+    @BeforeEach
+    void setupTest2() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
